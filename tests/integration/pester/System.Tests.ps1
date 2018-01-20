edba09a8112acd2657d79be884d0c9ab36d73a1a
Petrik van der Velde <petrikvandervelde@gmail.com>
2018-01-20 18:26:34 +1300

    Updating the integration tests for the new base image
    references #6

--- tests/integration/pester/System.Tests.ps1
+++ tests/integration/pester/System.Tests.ps1
@@ -1,7 +1,7 @@
 Describe 'On the system' {
     Context 'the machine name' {
         It 'should be a generated name with containing the version number and random characters' {
-            hostname | Should Match '^(cvproxyedge.*)-\d{1,2}-\d{1,3}-\d{1,3}-.{16}$'
+            hostname | Should Match '^(cv-proxy-edge.*)-\d{1,2}-\d{1,3}-\d{1,3}-.{16}$'
         }
     }
 
@@ -35,7 +35,7 @@ Describe 'On the system' {
 
     Context 'the administrator rights' {
         It 'should have default sudo settings' {
-            (Get-FileHash -Path /etc/sudoers -Algorithm SHA256).Hash | Should Be 'CC61F3AA6C9AF8F9540435AC280D6AD1AD0A734FDCAC6D855527F9944ABB67A3'
+            (Get-FileHash -Path /etc/sudoers -Algorithm SHA256).Hash | Should Be '1DA6E2BCBBA35669C9EB62370C88F4017686309C9AC4E6458D963321EAD42439'
         }
 
         It 'should not have additional sudo files' {
@@ -71,18 +71,86 @@ Describe 'On the system' {
         )
 
         It 'should have a file with updates' {
-            '/tmp/updates.txt' | Should Exist
+            '/test/updates.txt' | Should Exist
         }
 
-        $fileSize = (Get-Item '/tmp/updates.txt').Length
+        $fileSize = (Get-Item '/test/updates.txt').Length
         if ($fileSize -gt 0)
         {
-            $updates = Get-Content /tmp/updates.txt
+            $updates = Get-Content /test/updates.txt
             $additionalPackages = Compare-Object $allowedPackages $updates | Where-Object { $_.sideindicator -eq '=>' }
 
             It 'should all be installed' {
                 $additionalPackages.Length | Should Be 0
             }
+        }
+    }
+
+    Context 'system metrics' {
+        It 'with binaries in /usr/local/bin' {
+            '/usr/local/bin/scollector' | Should Exist
+        }
+
+        It 'with default configuration in /etc/scollector.d/scollector.toml' {
+            '/etc/scollector.d/scollector.toml' | Should Exist
+        }
+
+        $expectedContent = @'
+Host = "http://opentsdb.metrics.service.integrationtest:4242"
+
+[Tags]
+    environment = "test-integration"
+    os = "linux"
+
+'@
+        $scollectorConfigContent = Get-Content '/etc/scollector.d/scollector.toml' | Out-String
+        It 'with the expected content in the configuration file' {
+            $scollectorConfigContent | Should Be ($expectedContent -replace "`r", "")
+        }
+    }
+
+    Context 'has been daemonized' {
+        $serviceConfigurationPath = '/etc/systemd/system/scollector.service'
+        if (-not (Test-Path $serviceConfigurationPath))
+        {
+            It 'has a systemd configuration' {
+                $false | Should Be $true
+            }
+        }
+
+        $expectedContent = @'
+[Unit]
+Description=SCollector
+Requires=network-online.target
+After=network-online.target
+Documentation=http://bosun.org/scollector/
+
+[Install]
+WantedBy=multi-user.target
+
+[Service]
+ExecStart=/usr/local/bin/scollector -conf /etc/scollector.d/scollector.toml
+Restart=on-failure
+
+'@
+        $serviceFileContent = Get-Content $serviceConfigurationPath | Out-String
+        $systemctlOutput = & systemctl status scollector
+        It 'with a systemd service' {
+            $serviceFileContent | Should Be ($expectedContent -replace "`r", "")
+
+            $systemctlOutput | Should Not Be $null
+            $systemctlOutput.GetType().FullName | Should Be 'System.Object[]'
+            $systemctlOutput.Length | Should BeGreaterThan 3
+            $systemctlOutput[0] | Should Match 'scollector.service - SCollector'
+        }
+
+        It 'that is enabled' {
+            $systemctlOutput[1] | Should Match 'Loaded:\sloaded\s\(.*;\senabled;.*\)'
+
+        }
+
+        It 'and is running' {
+            $systemctlOutput[2] | Should Match 'Active:\sactive\s\(running\).*'
         }
     }
 }
