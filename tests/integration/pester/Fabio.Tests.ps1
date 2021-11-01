92062ce2a65b72bfda8ec57f65844ed0f38db407
Petrik van der Velde <petrikvandervelde@gmail.com>
2021-11-01 22:45:37 +1300

    Updating the integration tests

--- tests/integration/pester/Fabio.Tests.ps1
+++ tests/integration/pester/Fabio.Tests.ps1
@@ -1,24 +1,29 @@
+BeforeAll {
+    $serviceConfigurationPath = '/etc/systemd/system/fabio.service'
+    $beforeRulesContent = Get-Content '/etc/ufw/before.rules' | Out-String
+}
+
 Describe 'The fabio application' {
     Context 'is installed' {
         It 'with binaries in /usr/local/bin' {
-            '/usr/local/bin/fabio' | Should Exist
+            '/usr/local/bin/fabio' | Should -Exist
         }
 
         It 'with default configuration in /etc/fabio.d/fabio.properties' {
-            '/etc/fabio.d/fabio.properties' | Should Exist
+            '/etc/fabio.d/fabio.properties' | Should -Exist
         }
     }
 
     Context 'has been daemonized' {
-        $serviceConfigurationPath = '/etc/systemd/system/fabio.service'
-        if (-not (Test-Path $serviceConfigurationPath))
-        {
-            It 'has a systemd configuration' {
-               $false | Should Be $true
+        It 'has a systemd configuration' {
+            if (-not (Test-Path $serviceConfigurationPath))
+            {
+               $false | Should -Be $true
             }
         }
 
-        $expectedContent = @'
+        It 'with a systemd service' {
+            $expectedContent = @'
 [Service]
 ExecStart = /usr/local/bin/fabio -cfg /etc/fabio.d/fabio.properties
 RestartSec = 5
@@ -36,42 +41,42 @@ StartLimitIntervalSec = 0
 WantedBy = multi-user.target
 
 '@
-        $serviceFileContent = Get-Content $serviceConfigurationPath | Out-String
-        $systemctlOutput = & systemctl status fabio
-        It 'with a systemd service' {
-            $serviceFileContent | Should Be ($expectedContent -replace "`r", "")
+            $serviceFileContent = Get-Content $serviceConfigurationPath | Out-String
+            $serviceFileContent | Should -Be ($expectedContent -replace "`r", "")
 
-            $systemctlOutput | Should Not Be $null
-            $systemctlOutput.GetType().FullName | Should Be 'System.Object[]'
-            $systemctlOutput.Length | Should BeGreaterThan 3
-            $systemctlOutput[0] | Should Match 'fabio.service - fabio'
+            $systemctlOutput = & systemctl status fabio
+            $systemctlOutput | Should -Not -Be $null
+            $systemctlOutput.GetType().FullName | Should -Be 'System.Object[]'
+            $systemctlOutput.Length | Should -BeGreaterThan 3
+            $systemctlOutput[0] | Should -Match 'fabio.service - fabio'
         }
 
         It 'that is enabled' {
-            $systemctlOutput[1] | Should Match 'Loaded:\sloaded\s\(.*;\senabled;.*\)'
+            $systemctlOutput = & systemctl status fabio
+            $systemctlOutput[1] | Should -Match 'Loaded:\sloaded\s\(.*;\senabled;.*\)'
 
         }
 
         It 'and is running' {
-            $systemctlOutput[2] | Should Match 'Active:\sactive\s\(running\).*'
+            $systemctlOutput = & systemctl status fabio
+            $systemctlOutput[2] | Should -Match 'Active:\sactive\s\(running\).*'
         }
     }
 
     Context 'can be contacted' {
-        $response = Invoke-WebRequest -Uri http://localhost:9998 -UseBasicParsing
         It 'responds to HTTP calls' {
-            $response.StatusCode | Should Be 200
+            $response = Invoke-WebRequest -Uri http://localhost:9998 -UseBasicParsing
+            $response.StatusCode | Should -Be 200
         }
     }
 
     Context 'has a redirect setup' {
-        $beforeRulesContent = Get-Content '/etc/ufw/before.rules' | Out-String
         It 'for port 80' {
-            $beforeRulesContent | Should Match '-A PREROUTING -p tcp --dport 80 -j REDIRECT --to-port 7080'
+            $beforeRulesContent | Should -Match '-A PREROUTING -p tcp --dport 80 -j REDIRECT --to-port 7080'
         }
 
         It 'for port 443' {
-            $beforeRulesContent | Should Match '-A PREROUTING -p tcp --dport 443 -j REDIRECT --to-port 7443'
+            $beforeRulesContent | Should -Match '-A PREROUTING -p tcp --dport 443 -j REDIRECT --to-port 7443'
         }
     }
 }
